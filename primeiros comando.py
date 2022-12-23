0a70206e70f6a3d1ed750e7397c47f18953c1d07
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2022-12-23 09:33:32 -0300

    Create primeiros comando.py

---
+++ primeiros comando.py
@@ -0,0 +1,24 @@
+#Exibir alguma coisa:
+print('Olá, Mundo!')
+# soma entre dois valores
+print(7+4)
+# Quando os numeros estão entre aspas ele junta 
+print('7'+'4')
+#também mostra os dados, mas apenas números
+7+4
+'7'+'4'
+#para juntar números e caracteres é necessário usar uma virgula
+print('olá', 5) 
+
+#variáveis (obs: toda váriavel é um objeto)
+nome = 'Victória'
+idade = 17
+peso = 74.8
+print(nome, idade, peso)
+ 
+ # print escreve na tela 
+ # imput lê o que é escrito  
+nome = input ('Qual é o seu nome?')
+idade = input ('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+print(nome, idade, peso)
