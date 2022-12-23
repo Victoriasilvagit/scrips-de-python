e01959a448be348c108bbf8f8817ec329b23fb1e
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2022-12-23 10:11:04 -0300

    Create analise de antecessor e sucessor.py

---
+++ analise de antecessor e sucessor.py
@@ -0,0 +1,6 @@
+
+num = int(input('Digiteum número:'))
+ant = num - 1
+sus = num + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(num, ant, sus))
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(num, (num-1),(num+1)))
