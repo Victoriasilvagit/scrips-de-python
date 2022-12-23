2eb109e3fcb15189af0e59b06d82745afa6ecd8f
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2022-12-23 10:00:25 -0300

    Create atribuindo valores nas variaveis.py

---
+++ atribuindo valores nas variaveis.py
@@ -0,0 +1,15 @@
+#Desafio 01
+nome = input('Qual é o seu nome?')
+print('Olá,', nome, 'Prazer em te conhecer!')
+
+#Desafio 02
+dia = input ('Dia =')
+mes = input ('Mês =')
+ano = input ('Ano =')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano,'.', 'Correto?' )
+
+#Desafio 03 
+num1 = int(input('digite um número:'))
+num2= int(input('digite outro número:'))
+soma = num1+num2
+print('A soma é:', soma)
