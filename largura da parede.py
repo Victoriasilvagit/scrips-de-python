591d7b223bd95703f4d035b63faa28301c0a15f7
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2022-12-23 10:20:11 -0300

    Create largura da parede.py

---
+++ largura da parede.py
@@ -0,0 +1,6 @@
+#ex11
+
+l = float(input('Largura da parede:'))
+a = float (input('Altura da parede:'))
+area = l * a 
+print('A área da parede é {} e serão necessários {}l de  tinta para  pintar a parede:'.format(area, area/2))
